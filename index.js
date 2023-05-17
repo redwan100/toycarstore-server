eb40183a3a47b985d711bf660a99d1d79ea07da8
Redwan islam <redwanislam2760@gmail.com>
2023-05-17 21:50:37 +0600

    added mongodb server connected code

--- index.js
+++ index.js
@@ -10,7 +10,36 @@ const app = express();
  /* -------------------------------------------------------------------------- */
 app.use(express.json());
 app.use(cors());
+/* -------------------------------------------------------------------------- */
+/*                                  DATABASE                                  */
+/* -------------------------------------------------------------------------- */
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yq2vgbi.mongodb.net/?retryWrites=true&w=majority`;
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Database is Connected"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 
