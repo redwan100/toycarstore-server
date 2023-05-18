d138627942003c1924d3be5da5e197c1b0c0cc86
Redwan islam <redwanislam2760@gmail.com>
2023-05-19 04:00:59 +0600

    toy details route setup complete

--- index.js
+++ index.js
@@ -10,10 +10,11 @@ const app = express();
  /* -------------------------------------------------------------------------- */
 app.use(express.json());
 app.use(cors());
+
 /* -------------------------------------------------------------------------- */
 /*                                  DATABASE                                  */
 /* -------------------------------------------------------------------------- */
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yq2vgbi.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -35,22 +36,49 @@ async function run() {
     await client.db("admin").command({ ping: 1 });
     console.log("Toy Store Database is Connected");
 
-    // GET ROUTE
+    
+  /* -------------------------------------------------------------------------- */
+  /*                                  GET ROUTE                                 */
+  /* -------------------------------------------------------------------------- */
+
+    /* ------------------------------ GET ALL TOYS ------------------------------ */
     app.get("/allToys", async (req, res) => {
-      const result = await toyCollection.find().toArray();
+      const result = await toyCollection.find().limit(20).toArray();
 
       res.send(result);
     });
 
-    // POST ROUTE
+
+    app.get('/toyDetails/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id:new ObjectId(id)}
+      const result = await toyCollection.findOne(query)
+
+      res.send(result)
+    })
+
+    /* -------------------------- GET SUBCATEGORY TOYS -------------------------- */
+    app.get('/subcategory/:text', async (req, res) => {
+      //  const result = await toyCollection.find().limit(3).toArray()
+      //  res.send(result);
+      const category = req.query.text
+      console.log(category);
+      const cursor = toyCollection
+    })
+ 
+   /* -------------------------------------------------------------------------- */
+   /*                                 POST ROUTE                                 */
+   /* -------------------------------------------------------------------------- */
     app.post("/addToy", async (req, res) => {
       const body = req.body;
+      console.log(body);
 
-      const result = toyCollection.insertOne(body);
+      const result =await toyCollection.insertOne(body);
 
       res.send(result);
+
     });
-    
+
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
